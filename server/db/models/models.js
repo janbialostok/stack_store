4fe8250a788cb52e735c589fa43a17b7fb8de12a
Jan Bialostok <janbialostok@gmail.com>
2015-03-02 18:09:45 -0500

    Used mongoose model instead of variable inside module.exports

--- server/db/models/models.js
+++ server/db/models/models.js
@@ -4,8 +4,6 @@ mongoose.connect("mongodb://localhost/stack_store");
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongodb connection error: '));
 
-var Credit, Address, Review, Cart, User, Item;
-
 var Schema = mongoose.Schema;
 require("mongoose-currency").loadType(mongoose);
 var Currency = mongoose.Types.Currency;
@@ -85,18 +83,11 @@ itemSchema.pre('save', function (next){
     next();
 });
 
-Credit = mongoose.model('Credit', creditSchema);
-Address = mongoose.model('Address', addressSchema);
-Review = mongoose.model('Review', reviewSchema);
-Cart = mongoose.model('Cart', cartSchema);
-User = mongoose.model('User', userSchema);
-Item = mongoose.model('Item', itemSchema);
-
 module.exports = {
-    Credit: Credit,
-    Address: Address,
-    Review: Review,
-    Cart: Cart,
-    User: User,
-    Item: Item
+    Credit: mongoose.model('Credit', creditSchema),
+    Address: mongoose.model('Address', addressSchema),
+    Review: mongoose.model('Review', reviewSchema),
+    Cart: mongoose.model('Cart', cartSchema),
+    User: mongoose.model('User', userSchema),
+    Item: mongoose.model('Item', itemSchema)
 }
