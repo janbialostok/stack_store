103b8ad23047a4c24b045a27fd04c1bd75228cab
edenfull <spacecamper@gmail.com>
2015-03-02 17:08:05 -0500

    Implemented Address and Item models

--- server/db/models/models.js
+++ server/db/models/models.js
@@ -6,6 +6,8 @@ db.on('error', console.error.bind(console, 'mongodb connection error: '));
 var Credit, Address, Review, Cart, User, Item;
 
 var Schema = mongoose.Schema;
+require("mongoose-currency").loadType(mongoose);
+var Currency = mongoose.Types.Currency;
 
 var creditSchema = new Schema({
 	number: { type: String, required: true, unique: true },
@@ -15,7 +17,13 @@ var creditSchema = new Schema({
 });
 
 var addressSchema = new Schema({
-
+    address1: { type: String, required: true },
+    address2: String,
+    city: { type: String, required: true },
+    stateProv: { type: String, required: true },
+    postalCode: { type: String, required: true },
+    country: { type: String, required: true },
+    phone: { type: String, required: true }
 });
 
 var reviewSchema = new Schema({
@@ -35,7 +43,12 @@ var userSchema = new Schema({
 });
 
 var itemSchema = new Schema({
-
+    name: { type: String, required: true },
+    price: { type: Currency, required: true},
+    description: String,
+    image: String,
+    reviews: [reviewSchema],
+    sellerID: { type: Number, required: true }
 });
 
 Credit = mongoose.model('Credit', creditSchema);
