7d33d6000cf4012bc6802935e1331f89ff8a7e4d
Colin VanLang <colinvanlang@gmail.com>
2015-03-03 16:11:04 -0500

    fixed

--- browser/js/common/directives/itembanner/itembanner.js
+++ browser/js/common/directives/itembanner/itembanner.js
@@ -7,23 +7,23 @@ app.directive('itemBanner', function() {
 		scope: {
 			data: '='
 		},
-		// link: function(scope, elem, attr) {
-		// 	scope.extra = [];
-		// 	scope.active = [];
+		link: function(scope, elem, attr) {
+			scope.extra = [];
+			scope.active = [];
 
-		// 	function splitData(stepSize) {
-		// 		var index = 0;
-		// 		scope.extra = [];
-		// 		scope.active = scope.data.slice(index, stepSize);
-		// 		index += stepSize;
+			function splitData(stepSize) {
+				var index = 0;
+				scope.extra = [];
+				scope.active = scope.data.slice(index, stepSize);
+				index += stepSize;
 
-		// 		while (scope.data.length) {
-		// 			scope.extra.push(scope.data.slice(index,stepSize));
-		// 			index += stepSize;
-		// 		}
-		// 	}
+				while (index < scope.data.length) {
+					scope.extra.push(scope.data.slice(index,stepSize));
+					index += stepSize;
+				}
+			}
 
-		// 	splitData(4);
-		// }
+			splitData(4);
+		}
 	};
 });
