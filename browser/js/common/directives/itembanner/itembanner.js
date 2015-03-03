904cb84c5a6ddd5022bda58b4aa55595212b0795
Colin VanLang <colinvanlang@gmail.com>
2015-03-03 16:45:16 -0500

    responsive banner

--- browser/js/common/directives/itembanner/itembanner.js
+++ browser/js/common/directives/itembanner/itembanner.js
@@ -1,17 +1,29 @@
 'use strict';
 
-app.directive('itemBanner', function() {
+app.directive('itemBanner', function($window) {
 	return {
 		restrict: 'E',
 		templateUrl: 'js/common/directives/itembanner/itembanner.html',
 		scope: {
-			data: '='
+			data: '=',
+			title: '@'
 		},
 		link: function(scope, elem, attr) {
 			scope.extra = [];
 			scope.active = [];
+			updateBanner();
+
+			function updateBanner() {
+				var width = window.innerWidth;
+
+				if (width > 1200) splitData(4);
+				else if (width > 992) splitData(3);
+				else if (width > 768) splitData(2);
+				else splitData(1);
+			}
 
 			function splitData(stepSize) {
+				console.log(stepSize);
 				var index = 0;
 				scope.extra = [];
 				scope.active = scope.data.slice(index, stepSize);
@@ -22,7 +34,10 @@ app.directive('itemBanner', function() {
 				}
 			}
 
-			splitData(4);
+			$(window).resize(function() {
+				updateBanner();
+				scope.$digest();
+			});
 		}
 	};
 });
