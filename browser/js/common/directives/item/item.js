755615735da8acca663fa9f55d10a248475f206d
edenfull <spacecamper@gmail.com>
2015-03-03 17:43:46 -0500

    Added stars

--- browser/js/common/directives/item/item.js
+++ browser/js/common/directives/item/item.js
@@ -1,11 +1,26 @@
 'use strict';
 
 app.directive('item', function() {
-	return {
-		restrict: 'E',
-		templateUrl: 'js/common/directives/item/item.html',
-		scope: {
-			data: '='
+    return {
+	restrict: 'E',
+	templateUrl: 'js/common/directives/item/item.html',
+	scope: {
+	    data: '='
+	},
+	link: function(scope, elem, attr) {	    
+	    var repeat = function(num) {
+		var newArr = [];
+
+		for (var i = 0; i < num; i++) {
+		    newArr.push(i);
 		}
-	};
+
+		return newArr;
+	    };
+
+	    scope.fullStars = repeat(Math.floor(scope.data.rating));
+	    scope.emptyStars = repeat(Math.floor(5-scope.data.rating));
+	    scope.hasHalfStar = scope.fullStars.length + scope.emptyStars.length != 5;
+	}
+    };
 });
