2cc8f823af6d9b129aec2a9137afa53d62162c42
Jan Bialostok <janbialostok@gmail.com>
2015-03-05 18:45:23 -0500

    review create route modified

--- server/app/routes/review.js
+++ server/app/routes/review.js
@@ -2,34 +2,20 @@
 var router = require("express").Router();
 var mongoose = require("mongoose");
 var Review = mongoose.model("Review", Review);
-var User = mongoose.model("User", User);
 var Item = mongoose.model("Item", Item);
 
-router.use('/create/:itemid/:userid', function (req, res, next){
-	Item.findOne({ _id: req.params.itemid }, function (err, item){
-		if (err) next(err);
-		else {
-			req.item = item;
-			User.findOne({ _id: req.params.userid }, function (err, user){
-				if (err) next(err);
-				else {
-					req.userid = user._id;
-					next();
-				}
-			});
-		}
-	});
-});
-
-router.post('/create/:itemid/:userid', function (req, res, next){
+router.post('/create', function (req, res, next){
 	var review = new Review(req.body);
-	review.userId = req.userid;
-	review.productId = req.item._id;
 	review.save(function (err, newReview){
 		if (err) next(err);
 		else {
-			req.item.reviews.push(newReview._id);
-			res.status(200).end();
+			Item.findOne({ _id: req.body.itemid }, function (err, item){
+				if (err) res.status(400).end();
+				else {
+					item.reviews.push(newReview._id);
+					res.status(200).end();
+				}
+			});
 		}
 	});
 });
