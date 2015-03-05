afcced8869b685c7385c236f33bb6f656cb24dbb
covlllp <covlllp@users.noreply.github.com>
2015-03-05 16:56:35 -0500

    Update user.js

--- server/app/routes/user.js
+++ server/app/routes/user.js
@@ -3,13 +3,6 @@ var router = require("express").Router();
 var mongoose = require("mongoose");
 var User = mongoose.model("User", User);
 
-<<<<<<< HEAD
-// userRouter.get("/id", function(req, res) {
-//     User.findOne({ _id: req.query.userId }, function(err, user) {
-// 	if (!err) res.json(user);
-// 	else res.status(404).end();
-//     });
-// });
 
 userRouter.route('/find/:id')
 	.all(function (req, res, next){
