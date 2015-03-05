2f91ab5ad93bde32fd58db036179dc6a3064299e
Jan Bialostok <janbialostok@gmail.com>
2015-03-05 17:12:41 -0500

    Requested changes to item_page.js, item_page.html and item_page.scss made

--- browser/js/items/item_page.js
+++ browser/js/items/item_page.js
@@ -10,23 +10,10 @@ app.config(function($stateProvider) {
 
 app.controller('ItemCtrl', function($scope, $state, $stateParams, SingleItemFactory) {
 	SingleItemFactory.getItem($stateParams.productId).then(function (data){
-		console.log(data);
-		$scope.product = {
-			productId: data._id,
-			imgSrc: data.image,
-			productName: data.name,
-			price: data.price,
-			description: data.description,
-			quantity: data.quantity,
-			size: data.size,
-			tags: data.tags,
-			reviews: data.reviews
-		}
-		if ($scope.product.description === "") $scope.product.showDescription = false;
-		else $scope.product.showDescription = true;
+		$scope.item = data;
+		$scope.showDescription = $scope.item.description != "";
 		SingleItemFactory.getUser(data.sellerID).then(function (username){
-			$scope.product.sellerName = username;
+			$scope.item.sellerName = username;
 		});
 	});
-	$scope.item;
 });
