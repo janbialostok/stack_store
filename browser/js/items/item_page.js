7a29a239559e8bc9e5de641a0872a1495867b361
Jan Bialostok <janbialostok@gmail.com>
2015-03-05 11:18:07 -0500

    State for single item added directory browser/js/items, added express routes in server/routes/index.js

---
+++ browser/js/items/item_page.js
@@ -0,0 +1,53 @@
+"use strict";
+
+app.config(function($stateProvider) {
+	$stateProvider.state('single_item', {
+		url: '/item/:productId',
+		controller: 'ItemCtrl',
+		templateUrl: 'js/items/item_page.html'
+	});
+});
+
+app.factory("SingleItemFactory", function($http){
+	return {
+		getItem: function (productId){
+			var query = {};
+			if (productId) {
+				query.productId = productId;
+				return $http.get('/api/item/id', { params: query }).then(function (res){
+					return res.data;
+				});
+			}
+		},
+		getUser: function (userId){
+			var query = {};
+			if (userId){
+				query.userId = userId;
+				return $http.get('/api/user/id', { params: query }).then(function (res){
+					return res.data.name;
+				});
+			}
+		}
+	}
+});
+
+
+
+app.controller('ItemCtrl', function($scope, $state, $stateParams, SingleItemFactory) {
+	SingleItemFactory.getItem($stateParams.productId).then(function (data){
+		console.log(data);
+		$scope.product = {
+			imgSrc: data.image,
+			productName: data.name,
+			price: data.price,
+			description: data.description,
+			quantity: data.quantity,
+			size: data.size,
+			tags: data.tags,
+			reviews: data.reviews
+		}
+		SingleItemFactory.getUser(data.sellerID).then(function (username){
+			$scope.product.sellerName = username;
+		});
+	});
+});
--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,5 +1,21 @@
 'use strict';
 var router = require('express').Router();
+var Item = require('../../db/models/item.js');
+var User = require('../../db/models/user.js');
 module.exports = router;
 
 router.use('/tutorial', require('./tutorial'));
+
+router.get('/item/id', function(req, res){
+	Item.findOne({ _id: req.query.productId }, function(err, item){
+		if (!err) res.json(item);
+		else res.status(404).end();
+	});
+});
+
+router.get('/user/id', function(req, res){
+	User.findOne({ _id: req.query.userId }, function(err, user){
+		if (!err) res.json(user);
+		else res.status(404).end();
+	});
+});
