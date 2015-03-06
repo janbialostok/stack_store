90bcc30b3ed8af1f52f3c8b0f0cdbb69415cd833
Colin VanLang <colinvanlang@gmail.com>
2015-03-06 10:48:36 -0500

    categories directive done

--- browser/js/common/directives/categorymenu/catMenu.js
+++ browser/js/common/directives/categorymenu/catMenu.js
@@ -5,6 +5,7 @@ app.directive("categoryMenu", function(CategoryFactory){
 		restrict: 'E',
 		templateUrl: 'js/common/directives/categorymenu/catMenu.html',
 		scope: {
+			horiz: '='
 		},
 		link: function(scope, elem, attr){
 			scope.horiz = true;
@@ -13,15 +14,44 @@ app.directive("categoryMenu", function(CategoryFactory){
 	};
 });
 
-app.directive("categoryItem", function(){
+app.directive("categoryItem", function($compile){
 	return {
 		restrict: 'E',
-		templateUrl: 'js/common/directives/categorymenu/categoryItem.html',
+		template: '<li stop-propagation >{{ item.name }}</li>',
 		scope: {
 			item: '='
 		},
 		link: function(scope, elem, attr) {
-			scope.hasChildren = !!scope.item.children.length;
+			if (scope.item.children.length) {
+				var appendElem = '<category-collection collection="item.children">'
+								+ '</collection>';
+
+				$compile(appendElem)(scope, function(cloned, scope) {
+					elem.append(cloned);
+				});
+			}
 		}
 	};
+});
+
+app.directive('categoryCollection', function() {
+	return {
+		restrict: 'E',
+		scope: {
+			collection: '='
+		},
+		template: '<ul><category-item ng-repeat="item in collection" item="item">'
+				+ '</category-item></ul>'
+	};
+});
+
+app.directive('stopPropagation', function () {
+    return {
+        restrict: 'A',
+        link: function ($scope, elem, attr) {
+            elem.bind('click', function (e) {
+                e.stopPropagation();
+            });
+        }
+    };
 });
