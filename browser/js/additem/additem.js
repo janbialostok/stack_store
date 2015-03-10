ffc42e1402b8d93cc76eb0f764f254689afc9654
Colin VanLang <colinvanlang@gmail.com>
2015-03-09 20:55:11 -0400

    form tags close

--- browser/js/additem/additem.js
+++ browser/js/additem/additem.js
@@ -8,10 +8,19 @@ app.config(function($stateProvider) {
 	});
 });
 
-app.controller('AddItemCtrl', function($scope, CurrentFactory, $state) {
+app.controller('AddItemCtrl', function($scope, CurrentFactory, $state, CategoryFactory) {
 	if (!CurrentFactory.current.user.permLevel
 		|| CurrentFactory.current.user.permLevel === 'Guest')
 		$state.go('home');
 
-	
+	$scope.availTags = CategoryFactory.getFirstLevelOptions();
+	$scope.item = {
+		tags: []
+	};
+
+	$scope.addTag = function(tag) {
+		if ($scope.availTags.indexOf(tag) === -1) return;
+		$scope.item.tags.push(tag);
+		$scope.availTags = CategoryFactory.getChildrenFor(tag);
+	};
 });
