04546ef184a22fc3e8ec20f1e349d5d0ce4ab09e
Colin VanLang <colinvanlang@gmail.com>
2015-03-10 21:12:07 -0400

    save point

--- browser/js/cart/cart.js
+++ browser/js/cart/cart.js
@@ -47,7 +47,7 @@ app.controller('CartCtrl', function($scope, $state, $q, $timeout, CurrentFactory
 						CartFactory.updateCart($scope.cart._id, item).then(function (updated){
 							CurrentFactory.updateCartSize(user);
 							$scope.updatedMessage = true;
-							$timeout(function(){$scope.updatedMessage = false;}, 3000);
+							$timeout(function(){$scope.updatedMessage = false;}, 2000);
 						});
 					}
 				};
@@ -56,7 +56,7 @@ app.controller('CartCtrl', function($scope, $state, $q, $timeout, CurrentFactory
 						$scope.cart.items.splice(updated.index, 1);
 						CurrentFactory.updateCartSize(user);
 						$scope.updatedMessage = true;
-						$timeout(function(){$scope.updatedMessage = false;}, 3000);
+						$timeout(function(){$scope.updatedMessage = false;}, 2000);
 					});
 				};
 			});
